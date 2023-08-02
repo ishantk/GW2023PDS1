2e3cca780ed829c45aa45314441f77ce97ea6188
ishantk <er.ishant@gmail.com>
2023-08-02 13:05:31 +0530

    VetsApp - Add Customers for a Vet

--- Session19.py
+++ Session19.py
@@ -21,6 +21,11 @@ def register():
     return render_template('register.html')
 
 
+@web_app.route("/home")
+def home():
+    return render_template('home.html', email=session['vet_email'])
+
+
 @web_app.route("/register-vet", methods=['POST'])
 def register_vet():
 
@@ -35,7 +40,35 @@ def register_vet():
     db = MongoDBHelper(collection="vets")
     db.insert(vet_data)
 
-    return render_template('home.html')
+    return render_template('home.html', email=session['vet_email'])
+
+
+@web_app.route("/add-customer", methods=['POST'])
+def add_customer():
+
+    # if len(session['vet_id']) == 0:
+    #     return redirect("/")
+
+    customer_data = {
+        'name': request.form['name'],
+        'phone': request.form['phone'],
+        'email': request.form['email'],
+        'age': int(request.form['age']),
+        'gender': request.form['gender'],
+        'address': request.form['address'],
+        'vet_id': session['vet_id'],
+        'vet_email': session['vet_email'],
+        'createdOn': datetime.datetime.today()
+    }
+
+    if len(customer_data['name']) == 0 or len(customer_data['phone']) == 0 or len(customer_data['email']) == 0:
+        return render_template('error.html', message="Name, Phone and Email cannot be Empty")
+
+    print(customer_data)
+    db = MongoDBHelper(collection="customer")
+    db.insert(customer_data)
+
+    return render_template('success.html', message="{} added successfully".format(customer_data['name']))
 
 
 @web_app.route("/login-vet", methods=['POST'])
@@ -51,12 +84,25 @@ def login_vet():
     documents = list(db.fetch(vet_data))
     print(documents, type(documents))
     if len(documents) == 1:
-        return render_template('home.html')
+        session['vet_id'] = str(documents[0]['_id'])
+        session['vet_email'] = documents[0]['email']
+        session['vet_name'] = documents[0]['name']
+        print(vars(session))
+        return render_template('home.html', email=session['vet_email'], name=session['vet_name'])
     else:
         return render_template('error.html')
 
 
+@web_app.route("/logout")
+def logout():
+    session['vet_id'] = ""
+    session['vet_email'] = ""
+    return redirect("/")
+
+
 def main():
+    # In order to use session object in flask, we need to set some key as secret_key in app
+    web_app.secret_key = 'vetsapp-key-1'
     web_app.run()
 
 
